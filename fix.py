088785cdd713dc128dc599b8fd684a388d152657
jjsalam007 <abdussalamjamal007@gmail.com>
2025-04-26 17:41:30 +0200

    Initial commit

--- fix.py
+++ fix.py
@@ -3,19 +3,26 @@ import shutil
 from bs4 import BeautifulSoup
 import random
 
-def short_code_state(state_repl):
-    search_categories = open("/var/www/html/howardskeyandlock/duplicatescript1/staticfile/uscities - Sheet1.csv", "r", encoding="utf-8").readlines()
-    for credentials in search_categories:
-        credential= credentials.strip().split(',')
-        short_code = credential[2].replace('ï»¿', '').upper()
-        state_repl1= credential[3].replace('ï»¿', '').title()
-        try:
-            if state_repl.lower() == state_repl1.lower():
-                break
-        except:
-            break
-    return short_code
+preloader='''<link href="static/img/favicon.ico" rel="icon">
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
+   ym(101414846, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/101414846" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+</head>
+'''
 for d in os.listdir('pest-control'):
     try:
         for c in os.listdir(f'pest-control/{d}'):
@@ -23,61 +30,42 @@ for d in os.listdir('pest-control'):
                 for b in os.listdir(f'pest-control/{d}/{c}'):
                     try:
                         search_categories = open(f'pest-control/{d}/{c}/{b}', "r", encoding="utf8").read()
-                        op= search_categories.replace("", '')
-                        soup = BeautifulSoup(op, "lxml")
-                        state_repl= str(d).replace('ï»¿', '').replace('-', ' ').title()
-                        abbr_state= short_code_state(state_repl)
-                        city= str(c).replace('ï»¿', '').replace('-', ' ').title()
-                       
-                        op= op.replace('[State]', f'USA').replace('{BandD|B&amp;D}', random.choice(['B&D', 'BandD'])).replace('{‘bandd pest control’|BandD Pest Control|Band &amp; D Pest Control|B&amp;D Pest Control|bandd pest experts}', random.choice(['B&D Pest Control', 'BandD Pest Control', 'Band & D Pest Control', 'B&D Pest Control', 'bandd pest experts'])).replace('{and|&amp;|plus}', random.choice(['and', '&', 'plus'])).replace('{BandD Pest Control|B&amp;D Pest Control|B and D Pest Control}', random.choice(['BandD Pest Control', 'B&D Pest Control', 'B and D Pest Control']))
+                        op= search_categories.replace("﻿", '')
+                        op= op.replace('</head>', preloader.replace('"static/img', '"../../static/img'))
                         fp = open(f'pest-control/{d}/{c}/{b}', "w", encoding='utf-8-sig')
                         fp.writelines(op)
                         fp.close()
-                    except Exception as oops:
-                        print('Error communicating:', oops)
+                    except:
+                        pass
             except:
                 pass
     except:
         pass
-
 for d in os.listdir('pest-control'):
     try:
         search_categories = open(f'pest-control/{d}/index.html', "r", encoding="utf8").read()
-        if len(d.split('-')) >3:
-            op= search_categories.replace("", '')
-            soup = BeautifulSoup(op, "lxml")
-            split_data= d.split('-')
-            state_repl= f'{split_data[0]} {split_data[1]} {split_data[2]}'.replace('ï»¿', '').replace('-', ' ').title()
-
-            
-            op= op.replace('[State]', f'USA').replace('{BandD|B&amp;D}', random.choice(['B&D', 'BandD'])).replace('{‘bandd pest control’|BandD Pest Control|Band &amp; D Pest Control|B&amp;D Pest Control|bandd pest experts}', random.choice(['B&D Pest Control', 'BandD Pest Control', 'Band & D Pest Control', 'B&D Pest Control', 'bandd pest experts'])).replace('{and|&amp;|plus}', random.choice(['and', '&', 'plus'])).replace('{BandD Pest Control|B&amp;D Pest Control|B and D Pest Control}', random.choice(['BandD Pest Control', 'B&D Pest Control', 'B and D Pest Control']))
-
-            fp = open(f'pest-control/{d}/index.html', "w", encoding='utf-8-sig')
-            fp.writelines(op)
-            fp.close()
-        else:
-            op= search_categories.replace("", '')
-            soup = BeautifulSoup(op, "lxml")
-            split_data= d.split('-')
-            state_repl= d.replace('ï»¿', '').replace('-', ' ').title()
-
-            op= op.replace('[State]', f'USA').replace('{BandD|B&amp;D}', random.choice(['B&D', 'BandD'])).replace('{‘bandd pest control’|BandD Pest Control|Band &amp; D Pest Control|B&amp;D Pest Control|bandd pest experts}', random.choice(['B&D Pest Control', 'BandD Pest Control', 'Band & D Pest Control', 'B&D Pest Control', 'bandd pest experts'])).replace('{and|&amp;|plus}', random.choice(['and', '&', 'plus'])).replace('{BandD Pest Control|B&amp;D Pest Control|B and D Pest Control}', random.choice(['BandD Pest Control', 'B&D Pest Control', 'B and D Pest Control']))
-
-            fp = open(f'pest-control/{d}/index.html', "w", encoding='utf-8-sig')
-            fp.writelines(op)
-            fp.close()
+        op= search_categories.replace("﻿", '')
+        
+        op= op.replace('</head>', preloader.replace('"static/img', '"../static/img'))
+        fp = open(f'pest-control/{d}/index.html', "w", encoding='utf-8-sig')
+        fp.writelines(op)
+        fp.close()
     except:
         pass
-
-
 try:
     search_categories = open(f'pest-control/index.html', "r", encoding="utf8").read()
-
-    op= search_categories.replace("", '')
-    soup = BeautifulSoup(op, "lxml")
-    
-    op= op.replace('[State]', f'USA').replace('{BandD|B&amp;D}', random.choice(['B&D', 'BandD'])).replace('{‘bandd pest control’|BandD Pest Control|Band &amp; D Pest Control|B&amp;D Pest Control|bandd pest experts}', random.choice(['B&D Pest Control', 'BandD Pest Control', 'Band & D Pest Control', 'B&D Pest Control', 'bandd pest experts'])).replace('{and|&amp;|plus}', random.choice(['and', '&', 'plus'])).replace('{BandD Pest Control|B&amp;D Pest Control|B and D Pest Control}', random.choice(['BandD Pest Control', 'B&D Pest Control', 'B and D Pest Control']))
+    op= search_categories.replace("﻿", '')
+    op= op.replace('</head>', preloader)
     fp = open(f'pest-control/index.html', "w", encoding='utf-8-sig')
+    fp.writelines(op)
+    fp.close()
+except:
+    pass
+try:
+    search_categories = open(f'index.html', "r", encoding="utf8").read()
+    op= search_categories.replace("﻿", '')
+    op= op.replace('</head>', preloader)
+    fp = open(f'index.html', "w", encoding='utf-8-sig')
     fp.writelines(op)
     fp.close()
 except:
